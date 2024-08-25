73c30eb59f7573fee28d08a7605fa5569d8b4576
Pon <ponaravind.r@gmail.com>
2024-08-25 13:35:17 +0530

    test

---
+++ SysOutp.java
@@ -0,0 +1,28 @@
+package stringPrgms;
+
+import java.util.Arrays;
+
+public class SysOutp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n[] = {1,2,3,4};
+		int m[] = new int[n.length];
+		
+		for(int i=0;i<n.length;i++) {
+			m[i]=n[i];
+			System.out.println(m[i]);
+			
+			System.out.println(Arrays.toString(m));
+		}
+		System.out.println(Arrays.toString(m));
+		
+		System.out.println(Arrays.toString(m));
+	       // Test   
+
+		   //my changes
+		   System.out.println("Hi Ponaravindhan");
+	  }    
+	   
+	}
