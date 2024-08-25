e80e61d72692e32382863818a3bc9ecb3b7c6ee6
ponaravindhan <ponaravind.r@gmail.com>
2024-08-25 15:10:54 +0530

    jenkins1st file

--- test.java
+++ test.java
@@ -1,28 +1,12 @@
-package stringPrgms;
+package Jenkins;
 
 import java.util.Arrays;
 
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int n[] = {1,2,3,4};
-		int m[] = new int[n.length];
 		
-		for(int i=0;i<n.length;i++) {
-			m[i]=n[i];
-			System.out.println(m[i]);
-			
-			System.out.println(Arrays.toString(m));
-		}
-		System.out.println(Arrays.toString(m));
-		
-		System.out.println(Arrays.toString(m));
-	       // Test   
-
-		   //my changes
-		   System.out.println("Hi Ponaravindhan");
+		   System.out.println("my first jenkins - Hi Ponaravindhan");
 	  }    
 	   
 	}
